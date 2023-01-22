260a66b68976937a31b48dc66f03e05c1f7327e5
OpheNg <ophe.ng@live.fr>
2023-01-22 15:31:12 +0100

    my gallery page route ok

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get "drawings", to: 'pages#drawings'
   get "digital_paintings", to: 'pages#digital_paintings'
   get "photography", to: 'pages#photography'
+  get "my_gallery", to:'pages#my_gallery'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

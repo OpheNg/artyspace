1f123825565aaa892af4f627e31e546bfbd0afc3
OpheNg <ophe.ng@live.fr>
2023-01-22 17:40:50 +0100

    my gallery ok back

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,29 +34,33 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @user = current_user
-    @post.update(post_params_update)
-    redirect_to post(@post) if @post.update
-  end
 
-  def destroy
-    @post = Post.find(params[:id])
-    @user = current_user
     if @post.update(post_params_update)
       flash[:notice] = "post updated"
       redirect_to posts_path
     else
       render :edit
     end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @user = current_user
+    if @post.destroy
+      flash[:notice] = "post deleted"
+      redirect_to posts_path
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description, :user_id, :photo)
+    params.require(:post).permit(:title, :description, :user_id, :photo, :category)
   end
 
   def post_params_update
-    params.require(:post).permit(:title, :description, :photo)
+    params.require(:post).permit(:title, :description, :photo, :user_id, :category)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+
+  CATEGORIES = ['painting', 'drawing', 'digital painting', 'photography']
+  validates_inclusion_of :category, :in => CATEGORIES
 end

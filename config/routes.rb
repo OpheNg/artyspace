3173a52a8f85c5acd09d59322f5c936ed62123d4
OpheNg <ophe.ng@live.fr>
2023-01-22 15:23:27 +0100

    navbar modification, drop don menus

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get "dashboard", to: 'pages#dashboard'
+  get "paintings", to: 'pages#paintings'
+  get "drawings", to: 'pages#drawings'
+  get "digital_paintings", to: 'pages#digital_paintings'
+  get "photography", to: 'pages#photography'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
